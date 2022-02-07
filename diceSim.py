08138330b561a7477d04f38069426a8903c950ea
Krishpkreame <krishpkreame1405@gmail.com>
2022-02-07 17:47:16 +1300

    Update diceSim.py

--- diceSim.py
+++ diceSim.py
@@ -1,5 +1,40 @@
 import random
 randNumber = random.randint(1, 6)
 print(randNumber)
-if(randNumber == 6):
-    print("Dice rolled a dice!")
+
+if(randNumber == 1):
+    print("""
+
+ *
+
+    """)
+elif(randNumber == 2):
+    print("""
+*
+
+  *
+    """)
+elif(randNumber == 3):
+    print("""
+*
+ *
+  *
+    """)
+elif(randNumber == 4):
+    print("""
+* *
+
+* *
+    """)
+elif(randNumber == 5):
+    print("""
+* *
+ *
+* *
+    """)
+elif(randNumber == 6):
+    print("""
+* *
+* *
+* *
+    """)
